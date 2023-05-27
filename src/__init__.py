2901228c15893aa7a2cc89c5e694745f97902154
MrVeco <alexandre.douard@epitech.eu>
2023-05-27 17:31:42 +0200

    ADD: Action class with Action_type enum + rect handling

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,42 @@
 from subprocess import Popen, PIPE
 from copy import copy
-from typing import List
+from typing import List, Union
 from random import randint
-from json import loads
+from enum import Enum
+
+class Actions_type(Enum):
+    START = 1
+    ABOUT = 2
+    TURN = 3
+    BEGIN = 4
+    BOARD = 5
+    END = 6
+    RECTSTART = 7
+    RECTTURN = 8
+    RECTEND = 9
+    RECTBOARD = 10
+
+class Actions:
+    def __init__(self, name : str, args : List) -> None:
+        self.name = name
+        self.args = args
+
+    def __str__(self) -> str:
+        return f"{self.name} {' '.join([str(x) for x in self.args])}"
+
 
 class Player:
     def __init__(self, path):
         self.path = path
         self.process = Popen(path, stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-        #self.__get_about()
+        self.stdin : List[str] = []
+        self.stdout : List[str] = []
+        self.stderr : List[str] = []
+
+    def get_action(self, command: str, action: str = None):
+        self.process.stdin.write(command)
+        self.stdin.append(command)
+        self
 
     def set_size(self, size):
         self.process.stdin.write(f'START {size}\n')
@@ -30,7 +58,7 @@ class Player:
         return self.path
 
 class Board:
-    def __init__(self, load=None, size=20):
+    def __init__(self, load=None, x_size=None, y_size=None, size=20):
         if load:
             try:
                 with open(load, 'r') as f:
@@ -41,10 +69,25 @@ class Board:
                 raise Exception('The file {} is not a valid save file'.format(load))
             if sum([sum([1 for j in i if i not in [0, 1, 2]]) for i in self.board]) != 0:
                 raise Exception('The file {} is not a valid save file'.format(load))
+            self.x_size = len(self.board[0])
+            self.y_size = len(self.board)
+            if self.x_size == self.y_size:
+                self.size = self.x_size
+            else:
+                self.size = None
         else:
-            self.board = [[0 for _ in range(size)] for _ in range(size)]
-            self.size = size
-        if self.size < 5:
+            if not x_size:
+                self.x_size = size
+            else:
+                self.x_size = x_size
+            if not y_size:
+                y_size = size
+            else:
+                self.y_size = y_size
+            self.board = [[0 for _ in range(x_size)] for _ in range(y_size)]
+            if x_size != y_size:
+                self.size = None
+        if self.size < 5 or self.x_size < 5 or self.y_size < 5:
             raise Exception('The size of the board must be greater than 5')
 
     def __str__(self):
@@ -67,7 +110,8 @@ class Game:
             self.players.append(Player(player2))
         self.board : Board = Board(load=load, size=size)
         self.history : List[Board] = []
-        self.turn : int = -1
+        self.turn_history : List[Union[str, List[int]]] = ["BEGIN\n"]
+        self.turn : int = 0
         self.winner = None
 
     def load_board(self, load):
@@ -83,21 +127,11 @@ class Game:
                 if self.board[i][j] != 0 and self.board[i][j] == self.board[i + 1][j + 1] == self.board[i + 2][j + 2] == self.board[i + 3][j + 3] == self.board[i + 4][j + 4]:
                     return self.board[i][j]
         return None
-
+    
     def play_turn(self):
         player = self.turn % 2
-        self.history.append(copy(self.board))
-        self.players[player].process.stdin.write()
-        self.players[player].process.stdin.flush()
-        msg = self.players[player].process.stdout.readline().split()
-        if msg[0] != 'OK':
-            raise Exception(' '.join(msg[1:]))
-        x, y = map(int, msg[1:])
-        if self.board[x][y] != 0:
-            raise Exception('The move ({}, {}) is not valid'.format(x, y))
-        self.board[x][y] = player + 1
-        self.turn += 1
-        self.winner = self.check_winner()
+        last_turn = self.turn_history[-1]
+        
 
     def start_game(self):
         self.init_game()
