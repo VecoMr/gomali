b6533fbe6fd25883612b993120e9b4154d64da6e
MrVeco <alexandre.douard@epitech.eu>
2023-06-18 17:58:21 +0200

    ADD: timeout

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,12 @@
 from subprocess import Popen, PIPE
 from copy import copy
 from typing import List, Union
-from random import randint
+from random import shuffle
 from enum import Enum
+from time import sleep, time
+from re import match
+import selectors
+
 
 class Actions_type(Enum):
     START = 1
@@ -15,9 +19,18 @@ class Actions_type(Enum):
     RECTTURN = 8
     RECTEND = 9
     RECTBOARD = 10
+    OK = 11
+    ERROR = 12
+    TAKEBACK = 13
+    RESTART = 14
+    PLAY = 15
+    UNKNOW = 16
+    DEBUG = 17
+    SUGGEST = 18
 
 class Actions:
     def __init__(self, name : str, args : List) -> None:
+        self.type = Actions_type[name]
         self.name = name
         self.args = args
 
@@ -28,31 +41,50 @@ class Actions:
 class Player:
     def __init__(self, path):
         self.path = path
-        self.process = Popen(path, stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-        self.stdin : List[str] = []
+        self.process = Popen(path, stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True)
+        self.stdin : List[Actions] = []
         self.stdout : List[str] = []
         self.stderr : List[str] = []
 
-    def get_action(self, command: str, action: str = None):
+    def get_action(self, command: str, need_response : bool = True, action: str = None, timeout : int = 1) -> str:
+        print(f'GET action {command}')
         self.process.stdin.write(command)
         self.stdin.append(command)
-        self
+        self.process.stdin.flush()
+
+        output = ""
+        if need_response:
+            selector = selectors.DefaultSelector()
+            selector.register(self.process.stdout, selectors.EVENT_READ)
+            start_time = time()
+
+            while True:
+                elapsed_time = time() - start_time
+                if elapsed_time >= timeout:
+                    break
+                events = selector.select(timeout - elapsed_time)
+                if events:
+                    for key, _ in events:
+                        if key.fileobj is self.process.stdout:
+                            line = self.process.stdout.readline()
+                            if line == "":
+                                break
+                            else:
+                                output += line
+                else:
+                    elapsed_time = time() - start_time
+                    if elapsed_time >= timeout:
+                        break
+        print("output", output)
+        return output
 
     def set_size(self, size):
-        self.process.stdin.write(f'START {size}\n')
-        self.process.stdin.flush()
-        msg = self.process.stdout.readline().split()
-        if msg[0] != 'OK':
-            raise Exception(' '.join(msg[1:]))
+        msg = self.get_action(f"START {size}\n")
+        print("GET SIZE = ", msg)
 
     def get_about(self):
-        self.process.stdin.write('ABOUT\n')
-        self.process.stdin.flush()
-        msg = self.process.stdout.readline().split('"')
-        self.name = msg[1]
-        self.version = msg[3]
-        self.author = msg[5]
-        self.country = msg[7]
+        msg = self.get_action('ABOUT\n').split('"')
+        print("GET ABOUT = ", msg)
 
     def __str__(self):
         return self.path
@@ -76,17 +108,21 @@ class Board:
             else:
                 self.size = None
         else:
-            if not x_size:
+            if x_size == None:
                 self.x_size = size
             else:
                 self.x_size = x_size
-            if not y_size:
-                y_size = size
+            if y_size == None:
+                self.y_size = size
             else:
                 self.y_size = y_size
-            self.board = [[0 for _ in range(x_size)] for _ in range(y_size)]
-            if x_size != y_size:
-                self.size = None
+            self.board = [[0 for _ in range(self.x_size)] for _ in range(self.y_size)]
+            if self.x_size != self.y_size:
+                self.size = max(self.x_size, self.y_size)
+            else:
+                self.size = x_size
+            if not self.size:
+                self.size = max(self.x_size, self.y_size)
         if self.size < 5 or self.x_size < 5 or self.y_size < 5:
             raise Exception('The size of the board must be greater than 5')
 
@@ -99,15 +135,11 @@ class Board:
 
 class Game:
     def __init__(self, player1, player2, load=None, size=20, first='random'):
-        self.players : List[Player] = []
+        self.players : List[Player] = [Player(player1), Player(player2)]
         if first == 'random':
-            first = str(randint(1, 2))
-        if first == '1':
-            self.players.append(Player(player2))
-            self.players.append(Player(player1))
-        else:
-            self.players.append(Player(player1))
-            self.players.append(Player(player2))
+            shuffle(self.players)
+        if first == '2':
+            self.players = self.players[::-1]
         self.board : Board = Board(load=load, size=size)
         self.history : List[Board] = []
         self.turn_history : List[Union[str, List[int]]] = ["BEGIN\n"]
@@ -120,6 +152,7 @@ class Game:
     def init_game(self):
         [player.set_size(self.board.size) for player in self.players]
         [player.get_about() for player in self.players]
+        pass
 
     def check_winner(self):
         for i in range(self.board.size - 4):
@@ -127,14 +160,13 @@ class Game:
                 if self.board[i][j] != 0 and self.board[i][j] == self.board[i + 1][j + 1] == self.board[i + 2][j + 2] == self.board[i + 3][j + 3] == self.board[i + 4][j + 4]:
                     return self.board[i][j]
         return None
-    
+
     def play_turn(self):
         player = self.turn % 2
         last_turn = self.turn_history[-1]
-        
 
     def start_game(self):
         self.init_game()
-        # while not self.winner:
-        #     self.play_turn()
-        # print(f'Player {self.winner} won')
+        while not self.winner:
+            self.play_turn()
+        print(f'Player {self.winner} won')
